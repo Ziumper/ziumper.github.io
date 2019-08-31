9cdaa4ece4ebe70fa4a3c2272f543a3b18ccb0e4
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2019-08-31 20:04:19 +0200

    [FEATURE] Working file loader with css files.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import $ from "jquery";
+import "./assets/images/hacking.jpg"
+import './styles/test.css'
 
 $(document).ready(function() {
     $('.toggle').click(function() {
         $('.toggle').toggleClass('active');
     });
 
-})
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,27 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    plugins: [
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: devMode ? '[name].css' : '[name].[hash].css',
-            chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-        }),
-    ],
     module: {
         rules: [{
-            test: /\.less$/,
-            use: [
-                { loader: 'style-loader' },
-                {
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                        hmr: process.env.NODE_ENV === 'development',
-                    },
-                },
-                { loader: 'css-loader' },
-                { loader: 'less-loader' }
-            ],
-        }, ],
-    },
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    publicPath: 'dist',
+                }
+            }
+        ],
+
+    }
 };
