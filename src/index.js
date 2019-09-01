4238912271b6b6a59a68ffb977fbd06648c9d97c
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2019-09-01 14:04:57 +0200

    [FEATURE] Add babel loader.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './styles/main.scss';
 import $ from "jquery";
 import 'bootstrap';
 
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+
 
 $(document).ready(function() {
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
     },
     module: {
         rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
                 test: /\.s[ac]ss$/i,
                 use: [
                     MiniCssExtractPlugin.loader,
