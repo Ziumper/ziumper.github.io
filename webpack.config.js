e05dd7bb5749bb6efecfc61505fffdb22bf21bab
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2019-09-07 16:18:50 +0200

    [CLEANUP] Moved folders to be more structured.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        index: './src/index.js',
+        index: './src/scripts/index.js',
     },
     output: {
         filename: '[name].bundle.js',
