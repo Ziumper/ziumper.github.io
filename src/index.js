e5018d9db84155b96edfefab7ac344d6c21a5154
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2019-09-07 14:07:00 +0200

    [CLEANUP] Fix functions in main.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import './styles/main.scss';
 import $ from "jquery";
 import 'bootstrap';
 
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
-
 
 $(document).ready(function() {
 
@@ -14,18 +10,17 @@ $(document).ready(function() {
     function main() {
         addClickHandlerOnToggle();
         showPage();
-    }
 
+        function addClickHandlerOnToggle() {
+            $('.toggle').click(function() {
+                $('.toggle').toggleClass('active');
+            });
+        }
 
-    function addClickHandlerOnToggle() {
-        $('.toggle').click(function() {
-            $('.toggle').toggleClass('active');
-        });
-    }
-
-    function showPage() {
-        $('#loading').fadeOut(3000);
-        $('#page').fadeIn(4000);
+        function showPage() {
+            $('#loading').fadeOut(3000);
+            $('#page').fadeIn(4000);
+        }
     }
 
 });
