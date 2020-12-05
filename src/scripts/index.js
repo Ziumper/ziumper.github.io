ddb654e7583946c23eb71363805f445dc2425ecf
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-12-05 15:23:23 +0100

    fixed scrolling vertical

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,9 @@
 import '../styles/index.scss';
 import 'bootstrap';
+import 'lightbox2';
 import { main } from './main';
 import { filter } from './gridshuffle';
+
 main();
 
 window.filter = filter;
